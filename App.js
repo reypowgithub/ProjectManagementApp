aa8d871844fd24abc7fb0c5abbf0349e6818021c
reypowgithub <reypowofc@gmail.com>
2025-05-15 11:38:02 +0700

    feat : fitur login

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ActivityIndicator,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import "./global.css";
+import { useEffect, useState } from "react";
+import { loadFonts } from "./font";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function App() {
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await loadFonts();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSignIn = async () => {};
+
+  const handleSignUp = () => {};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 px-6 justify-center">
+      <Text className="text-3xl font-bold text-gray-900 mt-[67px] font-circular-bold w-64 mb-4">
+        Glad to meet you again!
+      </Text>
+
+      <View className="space-y-6">
+        <TextInput
+          className="w-full h-14 mb-8 px-4 bg-gray-50 bg-opacity-10 rounded-xl text-base text-gray-900"
+          placeholder="Email"
+          plaeceholderTextColor="#9ca3f"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <View className="flex-row items-center ">
+          <TextInput
+            className="w-full h-14 mb-8 px-4 bg-gray-50 bg-opacity-10 rounded-xl text-base text-gray-900"
+            placeholder="Password"
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <TouchableOpacity
+            onPress={() => setShowPassword(!showPassword)}
+            className="absolute right-4 bottom-1/2"
+          >
+            {showPassword ? (
+              <Ionicons name="eye-off" size={24} color="#9ca3af" />
+            ) : (
+              <Ionicons name="eye" size={24} color="#9ca3af" />
+            )}
+          </TouchableOpacity>
+        </View>
+
+        {error ? (
+          <Text className="text-red-500 text-center mb-2">{error}</Text>
+        ) : null}
+
+        <TouchableOpacity
+          className="w-full h-14 mb-4 bg-emerald-900 rounded-xl flex-row items-center justify-center"
+          onPress={handleSignIn}
+          disabled={loading}
+        >
+          {loading ? (
+            <ActivityIndicator color="#fff" />
+          ) : (
+            <Text className="text-white text-[16px] font-semibold">
+              Sign In
+            </Text>
+          )}
+        </TouchableOpacity>
+
+        <Text className="text-center text-[14px] text-black opacity-20 mb-4">
+          Or
+        </Text>
+
+        <TouchableOpacity
+          className="w-full h-14 mb-4 bg-amber-300 rounded-xl items-center justify-center"
+          onPress={handleSignUp}
+        >
+          <Text className="text-white text-[16px] font-semibold">Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/**@type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./App.js", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        "circular-black": "CircularStd-Black",
+        "circular-bold": "CircularStd-Bold",
+        "circular-book": "CircularStd-Book",
+        "circular-medium": "CircularStd-Medium",
+        "circular-light": "CircularStd-Light",
+        "circular-black-italic": "CircularStd-BlackItalic",
+        "circular-bold-italic": "CircularStd-BoldItalic",
+        "circular-book-italic": "CircularStd-BookItalic",
+        "circular-medium-italic": "CircularStd-MediumItalic",
+        "circular-light-italic": "CircularStd-LightItalic",
+        "manrope-bold": "Manrope-Bold",
+        "manrope-extra-bold": "Manrope-ExtraBold",
+        "manrope-extra-light": "Manrope-ExtraLight",
+        "manrope-light": "Manrope-Light",
+        "manrope-medium": "Manrope-Medium",
+        "manrope-regular": "Manrope-Regular",
+        "manrope-semi-bold": "Manrope-SemiBold",
+      },
+    },
+  },
+  plugins: [],
+};
