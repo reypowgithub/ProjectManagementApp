b067902d16d756d3d7bdade45727d0c9f53844a4
reypowgithub <reypowofc@gmail.com>
2025-05-16 13:44:51 +0700

    refactor : memperbaruhi agar jika sudah login bisa langsung ke homepage

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,30 @@
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { getAccessToken } from "../lib/auth";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
-  return <Redirect href={"/auth/login"} />;
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getToken = async () => {
+      const currentToken = await getAccessToken();
+      console.log("current token: ", currentToken);
+      setToken(currentToken);
+      setLoading(false);
+    };
+
+    getToken();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#000000" />
+      </View>
+    );
+  }
+
+  return <Redirect href={token ? "/(main)" : "/auth/login"} />;
 }
